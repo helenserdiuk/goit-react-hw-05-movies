d8dafac22da2721c570e65576d41eb26f67fec6b
Elena S <helenserdiuk@gmail.com>
2022-07-19 23:46:42 +0300

    fixed default baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,37 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+});
+
 const ROOT_KEY = '62e63fb69ff45b8c7b26a9723f0eb259';
 
 export async function getTrending() {
-  const { data } = await axios.get(`trending/movie/day?api_key=${ROOT_KEY}`);
+  const { data } = await instance.get(`trending/movie/day?api_key=${ROOT_KEY}`);
   return data.results;
 }
 
 export async function getSearchMovies(query) {
-  const { data } = await axios.get(
+  const { data } = await instance.get(
     `search/movie?api_key=${ROOT_KEY}&query=${query}`
   );
   return data.results;
 }
 
 export async function getMovieDetails(movieId) {
-  const { data } = await axios.get(`movie/${movieId}?api_key=${ROOT_KEY}`);
+  const { data } = await instance.get(`movie/${movieId}?api_key=${ROOT_KEY}`);
   return data;
 }
 
 export async function getMovieCredits(movieId) {
-  const { data } = await axios.get(
+  const { data } = await instance.get(
     `movie/${movieId}/credits?api_key=${ROOT_KEY}`
   );
   return data;
 }
 
 export async function getMovieReviews(movieId) {
-  const { data } = await axios.get(
+  const { data } = await instance.get(
     `movie/${movieId}/reviews?api_key=${ROOT_KEY}`
   );
   return data;
